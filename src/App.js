20bfbd55e72fd5a5dd96f1013fafc77ef5487a4f
Joris Onghena <joris@onghena.dev>
2019-11-01 17:58:18 +0100

    Reworked main app.js component
    Added menu and new logo

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
 import React from 'react';
-import logo from './logo.svg';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+import quiz from './quiz.png'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
+        <div class="container">
+          <div class="row">
+            <div class="col-md">
+              <img src={quiz} alt="quiz logo" class="App-logo"></img>
+            </div>
+          </div>
+          <div class="row">
+            <div class="col-sm">             
+            </div>
+            <div class="col-md">
+              <a href="/flags" class="btn btn-light btn-lg btn-block" role="button">Match flags with countries</a>   
+              <a href="/about" class="btn btn-light btn-lg btn-block" role="button">About</a>       
+            </div>
+            <div class="col-sm">              
+            </div>
+          </div>
+        </div>
+
       </header>
     </div>
   );
